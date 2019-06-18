ee86207b1497ddc1fd807efbc6a31f6f7bfde34f
Aurélie Nouaille-Degorce <aurelie@colom.be>
2019-06-18 10:10:53 +0200

    Initial Commit

---
+++ WavyDisplay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WavyDisplay
+//
+//  Created by Aurélie Nouaille-Degorce on 18/06/2019.
+//  Copyright © 2019 Nouaille-Degorce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
